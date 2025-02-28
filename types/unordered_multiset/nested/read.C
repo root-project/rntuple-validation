0cd7d4d10f3720c790d61ec4357c991439d4f855
Florine de Geus <florine.de.geus@cern.ch>
2025-02-28 15:05:25 +0100

    Add test for `std::unordered_multiset`

---
+++ types/unordered_multiset/nested/read.C
@@ -0,0 +1,105 @@
+#include <ROOT/REntry.hxx>
+#include <ROOT/RNTupleReader.hxx>
+
+using ROOT::Experimental::REntry;
+using ROOT::Experimental::RNTupleReader;
+
+#include <TSystem.h>
+
+#include <cstdint>
+#include <filesystem>
+#include <fstream>
+#include <ostream>
+#include <string>
+#include <string_view>
+#include <unordered_set>
+
+using UnorderedMultiset =
+    std::unordered_multiset<std::unordered_multiset<std::int32_t>>;
+
+static void PrintNestedUnorderedMultisetValue(const REntry &entry,
+                                              std::string_view name,
+                                              std::ostream &os,
+                                              bool last = false) {
+  UnorderedMultiset &value = *entry.GetPtr<UnorderedMultiset>(name);
+
+  std::vector<UnorderedMultiset::value_type> valueInnerSorted(value.begin(),
+                                                              value.end());
+  std::vector<std::vector<UnorderedMultiset::value_type::value_type>>
+      valueSorted;
+
+  for (auto inner : value) {
+    std::vector innerSorted(inner.begin(), inner.end());
+    std::sort(innerSorted.begin(), innerSorted.end());
+    valueSorted.push_back(innerSorted);
+}
+
+  os << "    \"" << name << "\": [";
+  bool outerFirst = true;
+  for (auto inner : valueSorted) {
+    if (outerFirst) {
+      outerFirst = false;
+    } else {
+      os << ",";
+    }
+    os << "\n      [";
+    bool innerFirst = true;
+    for (auto element : inner) {
+      if (innerFirst) {
+        innerFirst = false;
+      } else {
+        os << ",";
+      }
+      os << "\n        " << element;
+    }
+    if (!inner.empty()) {
+      os << "\n      ";
+    }
+    os << "]";
+  }
+  if (!valueSorted.empty()) {
+    os << "\n    ";
+  }
+  os << "]";
+  if (!last) {
+    os << ",";
+  }
+  os << "\n";
+}
+
+void read(std::string_view input = "types.unordered_multiset.nested.root",
+          std::string_view output = "types.unordered_multiset.nested.json") {
+  if (!std::filesystem::exists("libNestedUnorderedMultiset.so")) {
+    throw std::runtime_error("could not find the required ROOT dictionaries, "
+                             "please make sure to run `make` first");
+  }
+  gSystem->Load("libNestedUnorderedMultiset");
+
+  std::ofstream os(std::string{output});
+  os << "[\n";
+
+  auto reader = RNTupleReader::Open("ntpl", input);
+  auto &entry = reader->GetModel().GetDefaultEntry();
+  bool first = true;
+  for (auto index : *reader) {
+    reader->LoadEntry(index);
+
+    if (first) {
+      first = false;
+    } else {
+      os << ",\n";
+    }
+    os << "  {\n";
+
+    PrintNestedUnorderedMultisetValue(entry, "Index32", os);
+    PrintNestedUnorderedMultisetValue(entry, "Index64", os);
+    PrintNestedUnorderedMultisetValue(entry, "SplitIndex32", os);
+    PrintNestedUnorderedMultisetValue(entry, "SplitIndex64", os, /*last=*/true);
+
+    os << "  }";
+    // Newline is intentionally missing, may need to print a comma before the
+    // next entry.
+  }
+  os << "\n";
+  os << "]\n";
+}
