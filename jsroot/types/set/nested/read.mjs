e7027cd0374f042ff452e0e2d858c362258f4a4e
Jad Dayoub <jad.taim2003@gmail.com>
2026-08-07 15:13:50 +0200

    Add JSROOT test for types/set/nested

---
+++ jsroot/types/set/nested/read.mjs
@@ -0,0 +1,8 @@
+import { read, sortDeep } from "../../../jsroot_reader.mjs";
+
+const fields = ["Index32", "Index64", "SplitIndex32", "SplitIndex64"];
+
+const [input = "types.set.nested.root", output = "types.set.nested.json"] =
+  process.argv.slice(2);
+
+read(input, output, fields, sortDeep);
