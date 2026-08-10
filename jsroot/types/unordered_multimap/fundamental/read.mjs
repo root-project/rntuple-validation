59b7b2456d8d2138203b18377c56b900fe65f7e6
Jad Dayoub <jad.taim2003@gmail.com>
2026-08-10 10:02:08 +0200

    Add JSROOT test for types/unordered_multimap/fundamental

---
+++ jsroot/types/unordered_multimap/fundamental/read.mjs
@@ -0,0 +1,10 @@
+import { read, pairArrayToMap } from "../../../jsroot_reader.mjs";
+
+const fields = ["Index32", "Index64", "SplitIndex32", "SplitIndex64"];
+
+const [
+  input = "types.unordered_multimap.fundamental.root",
+  output = "types.unordered_multimap.fundamental.json",
+] = process.argv.slice(2);
+
+read(input, output, fields, pairArrayToMap);
