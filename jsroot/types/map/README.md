6fae7eb3b735fbdeb0678f3568db08700eded2d1
Jad Dayoub <jad.taim2003@gmail.com>
2026-08-11 10:31:56 +0200

    Add JSROOT test for types/map/nested

--- jsroot/types/map/README.md
+++ jsroot/types/map/README.md
@@ -1,3 +1,4 @@
 # `std::map`
 
 - [`fundamental`](fundamental): `std::map<std::string, std::int32_t>`
+- [`nested`](nested): `std::map<std::string, std::map<std::string, std::int32_t>>`
---
+++ jsroot/types/map/nested/read.mjs
@@ -0,0 +1,8 @@
+import { read, pairArrayToMap } from "../../../jsroot_reader.mjs";
+
+const fields = ["Index32", "Index64", "SplitIndex32", "SplitIndex64"];
+
+const [input = "types.map.nested.root", output = "types.map.nested.json"] =
+  process.argv.slice(2);
+
+read(input, output, fields, pairArrayToMap);
